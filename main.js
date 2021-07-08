468421fcfb02273e3e524e78695b3b672158dca9
kartikeyagr8 <87047153+kartikeyagr8@users.noreply.github.com>
2021-07-08 14:58:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var first_name = "Kartikeya";
+console.log(first_name);
+
+var last_name = "Bekkari";
+console.log(last_name);
+
+var full_name = first_name + last_name;
+console.log(full_name);
+
+var a = 10;
+console.log(a);
+
+var b = 5;
+console.log(b);
+
+var num = a + b;
+console.log(num);
